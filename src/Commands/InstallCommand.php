486e8eb0672ef99d714c1c29d512144e0f032ffe
Ivan Píša <info@ivanpisa.cz>
2022-10-08 20:51:21 +0200

    Add install command

---
+++ src/Commands/InstallCommand.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Rockero\StarterKit\Commands;
+
+use Illuminate\Console\Command;
+
+class InstallCommand extends Command
+{
+    public $signature = 'rockero:install';
+
+    public $description = 'Install starter kit';
+
+    public function handle(): void
+    {
+        $this->call("vendor:publish", ['--provider' => "Rockero\\StarterKit\\StarterKitServiceProvider"]);
+    }
+}
--- src/StarterKitServiceProvider.php
+++ src/StarterKitServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Rockero\StarterKit;
 
+use Rockero\StarterKit\Commands\InstallCommand;
 use Rockero\StarterKit\Commands\LintCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -12,6 +13,18 @@ class StarterKitServiceProvider extends PackageServiceProvider
     {
         $package
             ->name('starter-kit')
-            ->hasCommand(LintCommand::class);
+            ->hasCommand(LintCommand::class)
+            ->hasCommand(InstallCommand::class);
+    }
+
+    public function boot()
+    {
+        parent::boot();
+
+        $this->loadTranslationsFrom(__DIR__.'/../lang', 'courier');
+
+        $this->publishes([
+            __DIR__ . '/../tlint.json' => $this->app->basePath('tlint.json'),
+        ], );
     }
 }
