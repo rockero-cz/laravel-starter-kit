a02c1b0663ca05fa993650ef1cf9f9be2fbac0ef
Ivan Píša <info@ivanpisa.cz>
2023-03-11 10:57:48 +0100

    Add comments

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -6,10 +6,23 @@ use Illuminate\Console\Command;
 
 class InstallCommand extends Command
 {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
     public $signature = 'rockero:install';
 
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
     public $description = 'Install starter kit';
 
+    /**
+     * Execute the console command.
+     */
     public function handle(): void
     {
         if ($this->ask('Do you wish to setup CI?')) {
--- src/Facades/StarterKit.php
+++ src/Facades/StarterKit.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\Facade;
  */
 class StarterKit extends Facade
 {
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return \Rockero\StarterKit\StarterKit::class;
